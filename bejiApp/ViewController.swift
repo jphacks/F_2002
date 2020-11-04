9096744ae93757c45325ce906d4e4a0acbebaf7b
ikemoti <58681457+ikemoti@users.noreply.github.com>
2020-11-04 23:17:32 +0900

    Initial Commit

---
+++ bejiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bejiApp
+//
+//  Created by Sousuke Ikemoto on 2020/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
